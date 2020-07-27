572c1f89e735c4323d6e75f95df14cd5fecca323
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-07-27 00:00:00 +0000

    Improved use of Amazon S3 with module Archive Repertory.

--- src/File/ArchiveRepertory/FileManager.php
+++ src/File/ArchiveRepertory/FileManager.php
@@ -5,4 +5,53 @@ use ArchiveRepertory\File\FileManager as ArchiveRepertoryFileManager;
 
 class FileManager extends ArchiveRepertoryFileManager
 {
+    protected function createArchiveFolders($archiveFolder, $pathFolder = '')
+    {
+        // No need to create directories in Amazon: they don't exist (but it is
+        // possible to move and to remove them as prefix of files).
+        return true;
+    }
+
+    protected function createFolder($path)
+    {
+        // No need to create directory in Amazon.
+        return true;
+    }
+
+    protected function moveFile($source, $destination, $path = '')
+    {
+        $fileWriter = $this->getFileWriter();
+        $realSource = $this->concatWithSeparator($path, $source);
+        $realDestination = $this->concatWithSeparator($path, $destination);
+        if ($fileWriter->fileExists($realDestination)) {
+            return true;
+        }
+
+        if (!$fileWriter->fileExists($realSource)) {
+            $msg = sprintf(
+                $this->translate('Error during move of a file from "%s" to "%s" (local dir: "%s"): source does not exist.'),
+                $source,
+                $destination,
+                $path
+            );
+            $this->addError($msg);
+            return false;
+        }
+
+        try {
+            // No need to create directory in Amazon.
+            $result = $fileWriter->rename($realSource, $realDestination);
+        } catch (\Exception $e) {
+            $msg = sprintf(
+                $this->translate('Error during move of a file from "%s" to "%s" (local dir: "%s").'),
+                $source,
+                $destination,
+                $path
+            );
+            $this->addError($msg);
+            return false;
+        }
+
+        return $result;
+    }
 }
